fd09808c77fae5aa4a8a7f5e1587bc8526494b04
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2020-12-01 20:38:38 -0300

    Initial commit

---
+++ audiobook.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import  PyPDF2
+
+book = open('pequeno_principe.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+pages = pdf_reader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(0, pages):
+    page = pdf_reader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
